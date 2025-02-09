4f404ca8eb0f29fac7d2904d4ef517d4dc6f003c
radne0 <jmcornelius0@gmail.com>
2025-02-09 18:54:55 -0500

    cell implementation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 * Basic Window Implemented.
 * Basic Point Class
 * Basic Line Class and Line Drawing..
+* Maze Cell Class and Drawing
--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from window import Window
 from shapes import Point, Line
 from cell import Cell
 
+def some_path(my_win):
+    path =[
+         Cell(500,550,50,100,my_win,tw=True,bw=False,lw=True,rw=True),
+         Cell(500,550,100,150,my_win,tw=False,lw=True,rw=True,bw=False),
+         Cell(500,550,150,200,my_win,tw=False,lw=True,rw=True,bw=False),
+         Cell(500,550,200,250,my_win,tw=False,lw=True,rw=True,bw=False),
+         Cell(500,550,250,300,my_win,tw=False,lw=True,rw=False,bw=True),
+         Cell(550,600,250,300,my_win,tw=True,lw=False,rw=False,bw=True),
+         Cell(600,650,250,300,my_win,tw=True,lw=False,rw=False,bw=True),
+         Cell(650,700,250,300,my_win,tw=False,lw=False,rw=True,bw=True),
+         Cell(650,700,200,250,my_win,tw=False,lw=True,rw=True,bw=False),
+    ]
+
+
 
 def all_wall_types(my_win):
 
@@ -27,12 +41,14 @@ def all_wall_types(my_win):
     for cell in all_cells:
         cell.draw()
 
-
+    for line in path:
+        line.draw()
 
 
 def main():
     my_win = Window(800,600,"Maze Solver")
     all_wall_types(my_win)
+    some_path(my_win)
     my_win.wait_for_close()
 
 main()
