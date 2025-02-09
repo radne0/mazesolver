8c37767c1a6cffe3fd1f4240ae9c8a119b10eb72
radne0 <jmcornelius0@gmail.com>
2025-02-09 17:08:56 -0500

    refactor - split off Window class

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self,width, height,title="No Title"):
-        self.height = height
-        self.width = width
-        self.root = Tk()
-        self.root.title(title)
-        self.canvas = Canvas(self.root,width=self.width,height=self.height,bg="blue")
-        self.canvas.pack(fill=BOTH,expand=True)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW",self.close)        
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-
-
-    def wait_for_close(self):
-        self.running = True
-        while(self.running):
-            self.redraw()
-
-    def close(self):
-        self.running = False
-
-
+from window import Window
 
 def main():
     my_win = Window(800,600,"Maze Solver")
